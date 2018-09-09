17cdeb5098efb72c64cb15701f696fb4bec7163e
mamunsyuhada <imam.sy11@gmail.com>
2018-09-09 21:11:01 +0700

    struktur data

---
+++ app/src/main/java/id/mamun/layananmaskam/LayananData.java
@@ -0,0 +1,15 @@
+package id.mamun.layananmaskam;
+
+public class LayananData {
+    public static String[][] data = new String[][]{
+            {
+                "Nama Barang",
+                "tersedia",
+                "jumlah barang",
+                "lokasi icon",
+                "lokasi img",
+                "harga",
+                "deskripsi"
+            }
+    };
+}
