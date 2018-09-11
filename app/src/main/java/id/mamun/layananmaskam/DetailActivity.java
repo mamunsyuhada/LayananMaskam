85ac0a747162cf102e66ee93004d74139220bc81
mamunsyuhada <imam.sy11@gmail.com>
2018-09-11 14:45:40 +0700

    tambah aksi kirim WA ke staf layanan, menggunakan metode Uri

--- app/src/main/java/id/mamun/layananmaskam/DetailActivity.java
+++ app/src/main/java/id/mamun/layananmaskam/DetailActivity.java
@@ -1,8 +1,12 @@
 package id.mamun.layananmaskam;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -13,6 +17,16 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     public void aksiPesan(View view) {
+        try{
+            String text = "pinjam barang"; //TODO sesuai dengan barangnya
+            String toNumber = "6285723910307";
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("http://api.whatsapp.com/send?phone=" + toNumber + "&text=" + text));
+            startActivity(intent);
+        }catch (Exception e){
+            e.printStackTrace();
+//            Toast.makeText(DetailActivity.this, "Download aplikasi Whatsapp dulu", Toast.LENGTH_LONG).show();
+        }
 
     }
 }
